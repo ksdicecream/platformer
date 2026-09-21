a6f2d40473f8dd0b17829a65818f88afb2c0eb2e
ksdicecream <ksdicecream@gmail.com>
2026-09-21 15:29:19 -0700

    added background, jump sound, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Platformer for Jumpstart
+
+Used WASD or arrow keps to reach the flag!
+
+Background from Kenney Game Assets New Platformer Pack (https://kenney.nl/assets/new-platformer-pack)
+
+Cat sprite drawn by me.
--- player.gd
+++ player.gd
@@ -13,6 +13,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AudioStreamPlayer.play()
 
 
 	# Get the input direction and handle the movement/deceleration.
